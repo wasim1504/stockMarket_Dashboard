61b389e84740b51b3e9c7257b56c345059f47648
Md Wasim Ahmad <wasimahmad15042000@gmail.com>
2023-12-27 16:16:51 +0530

    changes in create account flow

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,14 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signup as signupApi } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
 
+import { useNavigate } from "react-router-dom";
+
 export function useSignup() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
       toast.success(
         "Account successfully created! Please verufy the new account from the user's email address."
       );
+      navigate("/login", { replace: true });
     },
   });
 
